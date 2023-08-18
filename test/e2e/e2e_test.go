77afd22dc67bd1c7f383d435ff2dbe2e039f7543
Robert Wittman <robkwittman@gmail.com>
2023-08-18 01:09:35 -0400

    E2E tests are running, RBAC issues need to be fixed

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -32,7 +32,7 @@ import (
 )
 
 // constant parts of the file
-const namespace = "podinfo-operator"
+const namespace = "podinfo-operator-system"
 
 // NOTE: The resources were deploying here are very similar to the
 // memcached-operator example provided by the operator-sdk. Most of this was borrowed
